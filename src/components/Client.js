2573feb132a7385525aebca2410dedd407b4ca67
webrish <rishwebd@gmail.com>
2021-07-28 10:41:37 +0530

    minor changes

--- src/components/Client.js
+++ src/components/Client.js
@@ -1,16 +1,81 @@
-import React from 'react'
+import {
+  Container,
+  Paper,
+  TableContainer,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Box,
+} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
 import PropTypes from 'prop-types'
+import Checkbox from '@material-ui/core/Checkbox'
+import EditIcon from '@material-ui/icons/Edit'
+import DeleteIcon from '@material-ui/icons/Delete'
 
-const Client = props => {
-    return (
-        <div>
-            this is client
-        </div>
-    )
+const useStyles = makeStyles(() => ({
+  root: {
+    // display: 'flex',
+    width: '700px',
+    height: '620px',
+    // marginTop: '90px',
+    marginLeft: '30px',
+  },
+}))
+
+const Client = (props) => {
+  const classes = useStyles()
+  return (
+    <>
+      <div style={{ display: 'flex' }}>
+        <Container className={classes.root}>
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell padding='checkbox'>
+                    <Checkbox />
+                  </TableCell>
+                  <TableCell>Client Name</TableCell>
+                  <TableCell>Email ID</TableCell>
+                  <TableCell>Action</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell padding='checkbox'>
+                    <Checkbox />
+                  </TableCell>
+                  <TableCell>John Doe</TableCell>
+                  <TableCell>john@example.com</TableCell>
+                  <TableCell>
+                    <EditIcon />
+                    <DeleteIcon />
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell padding='checkbox'>
+                    <Checkbox />
+                  </TableCell>
+                  <TableCell>Jane Doe</TableCell>
+                  <TableCell>jane@example.com</TableCell>
+                  <TableCell>
+                    <EditIcon />
+                    <DeleteIcon />
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Container>
+        <Container>Hello</Container>
+      </div>
+    </>
+  )
 }
 
-Client.propTypes = {
-
-}
+Client.propTypes = {}
 
 export default Client
---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Avatar from '@material-ui/core/Avatar'
+const Navbar = ({ page }) => {
+  return (
+    <>
+      <ul>
+        <li className='nav1'>{page}</li>
+
+        <li className='nav2' style={{ float: 'right' }}>
+          Aman Sikarwar{' '}
+        </li>
+        <li className='nav3' style={{ float: 'right' }}>
+          <Avatar>AS</Avatar>
+        </li>
+      </ul>
+    </>
+  )
+}
+
+Navbar.propTypes = {}
+
+export default Navbar
