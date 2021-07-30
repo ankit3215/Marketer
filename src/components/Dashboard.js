d0265291a1245a649fbbdad21f1df3e8612abd9c
aman2698 <sikarwaraman26@gmail.com>
2021-07-30 15:33:40 +0530

    add dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { useDispatch, useSelector } from 'react-redux'
 import { Container, Typography, Paper, Grid, Box, Divider} from "@material-ui/core";
 import CallToActionIcon from '@material-ui/icons/CallToAction';
 import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
@@ -9,8 +9,27 @@ import { Directions } from "@material-ui/icons";
 import PropTypes from 'prop-types'
 import Navbar from "./Navbar";
 import "./CSS/mailer.css";
+import { CompaignList } from '../redux/actionCreators/compaignsAction';
+import { clientList } from '../redux/actionCreators/clientAction'
 
 const Dashboard = props => {
+    const dispatch = useDispatch();
+    const [clientLen, setClientLen] = React.useState("0");
+
+    const [campaignLen, setCampaignLen] = React.useState("0");
+    const client = useSelector((state) => state.ClientReducer);
+    const {campaigns} = useSelector((state) => state.CampaignReducer);
+
+    React.useEffect(() => {
+        dispatch(clientList())
+        setClientLen(client?.clients?.length)
+        dispatch(CompaignList())
+        setCampaignLen(campaigns?.length)
+        // console.log("campaigns",campaigns?.length)
+        // console.log("client",client.clients.length)
+      }, [])
+
+    
     return (
         <div>
         <Navbar page='Dashboard' />
@@ -31,7 +50,7 @@ const Dashboard = props => {
                           
                             Total Campaigns
                            
-                            <h3 className="" style={{marginTop:"2px"}}>04</h3>
+                            <h3 className="" style={{marginTop:"2px"}}>{campaignLen<=9?`0${campaignLen}`:campaignLen}</h3>
                            
                      </div>
                     </div>
@@ -57,7 +76,7 @@ const Dashboard = props => {
                           
                             Total Clients
                            
-                            <h3 className="" style={{marginTop:"2px"}}>234</h3>
+                            <h3 className="" style={{marginTop:"2px"}}>{clientLen<=9?`0${clientLen}`:clientLen}</h3>
                            
                      </div>
                     </div>
