3d382c55e2fe1791b74e197ba15c9a03646a7c93
webrish <rishwebd@gmail.com>
2021-08-06 14:43:58 +0530

    minor changes

--- src/components/tables/CompaignsTable.js
+++ src/components/tables/CompaignsTable.js
@@ -1,60 +1,77 @@
-import React, { useState, forwardRef, useRef, useImperativeHandle,useEffect } from "react";
-import "antd/dist/antd.css";
-import "../../index.css";
-import { Table, Input, Button, Space } from "antd";
-import Highlighter from "react-highlight-words";
-import { SearchOutlined } from "@ant-design/icons";
-import IconButton from "@material-ui/core/IconButton";
-import DeleteIcon from "@material-ui/icons/Delete";
-import EditIcon from "@material-ui/icons/Edit";
+import React, {
+  useState,
+  forwardRef,
+  useRef,
+  useImperativeHandle,
+  useEffect,
+} from 'react'
+import 'antd/dist/antd.css'
+import '../../index.css'
+import { Table, Input, Button, Space } from 'antd'
+import Highlighter from 'react-highlight-words'
+import { SearchOutlined } from '@ant-design/icons'
+import IconButton from '@material-ui/core/IconButton'
+import DeleteIcon from '@material-ui/icons/Delete'
+import EditIcon from '@material-ui/icons/Edit'
 import { useDispatch, useSelector } from 'react-redux'
-import { clientList,editClient,deleteClient } from '../../redux/actionCreators/clientAction'
+import {
+  clientList,
+  editClient,
+  deleteClient,
+} from '../../redux/actionCreators/clientAction'
 import Modal from '../../common/Modal'
 
 import { CompaignList } from '../../redux/actionCreators/compaignsAction'
-import {editCampaign,deleteCampaign} from '../../redux/actionCreators/campaignsActions';
-export  const CompaignsTable = () => {
-    const [filter, setfilter] = useState({
-        searchText: "",
-        searchedColumn: "",
-      });
-      const [isModal, setIsModal] = useState(false)
-      const [formData, setFormData] = React.useState({
-        name: '',
-       subject: '',
-       content: '',
-      })
-      const [UserID, setUserID] = React.useState('')
-      const [data, setData] = useState([])
+import {
+  editCampaign,
+  deleteCampaign,
+} from '../../redux/actionCreators/campaignsActions'
+export const CompaignsTable = () => {
+  const [filter, setfilter] = useState({
+    searchText: '',
+    searchedColumn: '',
+  })
+  const [isModal, setIsModal] = useState(false)
+  const [formData, setFormData] = React.useState({
+    name: '',
+    subject: '',
+    content: '',
+  })
+  const [UserID, setUserID] = React.useState('')
+  const [data, setData] = useState([])
 
-      const dispatch = useDispatch();
+  const dispatch = useDispatch()
   const { campaigns } = useSelector((state) => state.CampaignReducer)
 
   const openModal = (row) => {
-    setFormData({ ...formData, name: row.name, subject: row.subject, content: row.content })
+    setFormData({
+      ...formData,
+      name: row.name,
+      subject: row.subject,
+      content: row.content,
+    })
     setUserID(row.key)
     setIsModal(!isModal)
   }
   useEffect(() => {
-    if(campaigns&&campaigns){
-     let clientData= campaigns.map(e => {
-        return {key : e.id,...e.data}
+    if (campaigns && campaigns) {
+      let clientData = campaigns.map((e) => {
+        return { key: e.id, ...e.data }
       })
       setData(clientData)
-      
+
       console.log(clientData)
     }
   }, [campaigns])
   useEffect(() => {
     dispatch(CompaignList())
   }, [])
-  const searchInput = useRef();
+  const searchInput = useRef()
 
-  const handleDelete =async (row) =>{
+  const handleDelete = async (row) => {
     await dispatch(deleteCampaign(row.key))
-     
-   }
-   const toggle = () => {
+  }
+  const toggle = () => {
     setIsModal(!isModal)
   }
   const onChange = (e) => {
@@ -68,20 +85,19 @@ export  const CompaignsTable = () => {
     toggle()
   }
 
-  
   const handleSearch = (selectedKeys, confirm, dataIndex) => {
-    confirm();
+    confirm()
     setfilter({
       ...filter,
       searchText: selectedKeys[0],
       searchedColumn: dataIndex,
-    });
-  };
+    })
+  }
 
-  const handleReset = async(clearFilters) => {
-   await clearFilters();
-    setfilter({ ...filter, searchText: "" });
-  };
+  const handleReset = async (clearFilters) => {
+    await clearFilters()
+    setfilter({ ...filter, searchText: '' })
+  }
 
   const getColumnSearchProps = (dataIndex) => ({
     filterDropdown: ({
@@ -93,7 +109,7 @@ export  const CompaignsTable = () => {
       <div style={{ padding: 8 }}>
         <Input
           ref={(node) => {
-            searchInput.current = node;
+            searchInput.current = node
           }}
           placeholder={`Search ${dataIndex}`}
           value={selectedKeys[0]}
@@ -101,36 +117,36 @@ export  const CompaignsTable = () => {
             setSelectedKeys(e.target.value ? [e.target.value] : [])
           }
           onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
-          style={{ marginBottom: 8, display: "block" }}
+          style={{ marginBottom: 8, display: 'block' }}
         />
         <Space>
           <Button
-            type="primary"
+            type='primary'
             onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
             icon={<SearchOutlined />}
-            size="small"
+            size='small'
             style={{ width: 90 }}
           >
             Search
           </Button>
           <Button
-          id="reset"
+            id='reset'
             onClick={() => handleReset(clearFilters)}
-            size="small"
+            size='small'
             style={{ width: 90 }}
           >
             Reset
           </Button>
           <Button
-            type="link"
-            size="small"
+            type='link'
+            size='small'
             onClick={() => {
-              confirm({ closeDropdown: false });
+              confirm({ closeDropdown: false })
               setfilter({
                 ...filter,
                 searchText: selectedKeys[0],
                 searchedColumn: dataIndex,
-              });
+              })
             }}
           >
             Filter
@@ -139,7 +155,7 @@ export  const CompaignsTable = () => {
       </div>
     ),
     filterIcon: (filtered) => (
-      <SearchOutlined style={{ color: filtered ? "#1890ff" : undefined }} />
+      <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />
     ),
     onFilter: (value, record) =>
       record[dataIndex]
@@ -147,88 +163,92 @@ export  const CompaignsTable = () => {
             .toString()
             .toLowerCase()
             .includes(value.toLowerCase())
-        : "",
+        : '',
     onFilterDropdownVisibleChange: (visible) => {
       if (visible) {
-        setTimeout(() => searchInput.current.select(), 100);
+        setTimeout(() => searchInput.current.select(), 100)
       }
     },
     render: (text) =>
       filter.searchedColumn === dataIndex ? (
         <Highlighter
-          highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
+          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
           searchWords={[filter.searchText]}
           autoEscape
-          textToHighlight={text ? text.toString() : ""}
+          textToHighlight={text ? text.toString() : ''}
         />
       ) : (
         text
       ),
-  });
+  })
   const columns = [
     {
-      title: "Name",
-      dataIndex: "imageURL",
-      key: "imageURL",
-      width: "20%",
-      render: (text,record) => (
+      title: 'Image',
+      dataIndex: 'imageURL',
+      key: 'imageURL',
+      width: '20%',
+      render: (text, record) => (
         <>
-          <img width="50px" height="50px" style={{borderRadius:"25px"}} src={text} />
+          <img
+            width='50px'
+            height='50px'
+            style={{ borderRadius: '25px' }}
+            src={text}
+          />
         </>
-      )
+      ),
       // ...getColumnSearchProps("name"),
     },
     {
-      title: "Name",
-      dataIndex: "name",
-      key: "name",
-      width: "20%",
-      ...getColumnSearchProps("name"),
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      width: '20%',
+      ...getColumnSearchProps('name'),
     },
     {
-      title: "Subject",
-      dataIndex: "subject",
-      key: "subject",
-      width: "20%",
-      ...getColumnSearchProps("subject"),
+      title: 'Subject',
+      dataIndex: 'subject',
+      key: 'subject',
+      width: '20%',
+      ...getColumnSearchProps('subject'),
     },
     {
-      title: "Content",
-      dataIndex: "content",
-      key: "content",
-      width: "30%",
+      title: 'Content',
+      dataIndex: 'content',
+      key: 'content',
+      width: '30%',
       // ...getColumnSearchProps("content"),
     },
-   
-   
+
     {
-      title: "Action",
-      width: "20%",
-      render: (text,record) => (
+      title: 'Action',
+      width: '20%',
+      render: (text, record) => (
         <div>
           <IconButton onClick={() => openModal(record)}>
-            {" "}
+            {' '}
             <EditIcon />
           </IconButton>
 
-          <IconButton onClick={() =>  handleDelete(record)}>
-            {" "}
+          <IconButton onClick={() => handleDelete(record)}>
+            {' '}
             <DeleteIcon />
           </IconButton>
         </div>
-      )
+      ),
     },
-  ];
-    return (
-        <div>
-             <Table
-        style={{ maxHeight: "100px" }}
+  ]
+  return (
+    <div>
+      <Table
+        style={{ maxHeight: '100px' }}
         columns={columns}
         dataSource={data}
         pagination={false}
-        scroll={{ y: "350px" }}
-        size="middle"
-        loading={data.length>0?false:true}
+        scroll={{ y: '350px' }}
+        size='middle'
+        loading={data.length > 0 ? false : true}
       />
       <Modal on={isModal} toggle={toggle}>
         {isModal && (
@@ -266,6 +286,6 @@ export  const CompaignsTable = () => {
           </form>
         )}
       </Modal>
-        </div>
-    )
+    </div>
+  )
 }
